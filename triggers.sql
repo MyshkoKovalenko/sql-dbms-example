ee2d2480169b6d269645ada6952792726cf4ed1e
Mykhailo Kovalenko <mykhaylo2007@gmail.com>
2026-06-23 18:46:49 +0300

    Added log_order trigger

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,7 @@
-DROP TRIGGER IF EXISTS update_order_total ON order_items;
-DROP FUNCTION IF EXISTS update_all_orders;
+DROP FUNCTION IF EXISTS update_all_orders CASCADE;
+DROP TRIGGER IF EXISTS t_update_order_total ON order_items;
+DROP FUNCTION IF EXISTS log_order CASCADE;
+DROP TRIGGER IF EXISTS t_log_order ON orders;
 
 CREATE OR REPLACE FUNCTION update_all_orders()
 RETURNS TRIGGER
@@ -15,9 +17,28 @@ AS $$
     END;
 $$;
 
-CREATE OR REPLACE TRIGGER update_order_total AFTER
+CREATE OR REPLACE TRIGGER t_update_order_total AFTER
     INSERT OR
     UPDATE OR
     DELETE
 ON order_items FOR EACH ROW
-EXECUTE PROCEDURE update_all_orders();
+EXECUTE FUNCTION update_all_orders();
+
+CREATE OR REPLACE FUNCTION log_order()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+
+    INSERT INTO order_log (order_id, customer_id, action, log_date)
+        SELECT NEW.order_id, NEW.customer_id, 'Purchase', NEW.order_date;
+    RETURN NEW;
+
+    COMMIT;
+    END;
+$$;
+
+CREATE OR REPLACE TRIGGER t_log_order AFTER
+    INSERT
+ON orders FOR EACH ROW
+EXECUTE FUNCTION log_order();
